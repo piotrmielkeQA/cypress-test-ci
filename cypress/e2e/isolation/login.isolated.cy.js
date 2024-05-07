d4da179babf7e47506cf8806969f4ccb12ce25be
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-07 08:28:41 +0200

    er04

--- cypress/e2e/isolation/login.isolated.cy.js
+++ cypress/e2e/isolation/login.isolated.cy.js
@@ -25,4 +25,26 @@ describe('Login tests in isolation', () => {
         cy.get('h1').should('contain.text', fakeLoginResponse.firstName)
     })
 
+    it('should fail to login', () => {
+        const message = "Invalid username/password supplied"
+        cy.intercept('POST', '**/users/signin', (req) => {
+            req.reply({
+                statusCode: 422,
+                body: {
+                    timestamp: "2024-05-06T15:48:46.127+00:00",
+                    status: 422,
+                    error: "Unprocessable Entity",
+                    message: message,
+                    path: "/users/signin"
+                }
+            })
+        })
+
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('password')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert-danger').should('have.text', message)
+    })
+
 })
